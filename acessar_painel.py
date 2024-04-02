175bcb17212f8b2d0c674d99ff80435e8ed3841d
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-04-02 09:44:17 -0300

    Adicionada a funcionalidade de finalizar o programa

--- acessar_painel.py
+++ acessar_painel.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pyautogui
+import keyboard
+import sys
 
 class acessar_painel:
     def __init__(self,credenciais:tuple):
         self.credeciais = credenciais
+        
+    def sair_programa(self):
+        sys.exit()
         
     def abrir_navegador(self):
         navegador = webdriver.Chrome()
@@ -17,5 +22,4 @@ class acessar_painel:
         entrar.click()
         navegador.get(self.credeciais.get("url"))
         pyautogui.press("f11")
-        while True:
-            continue
+        keyboard.wait("f11")
