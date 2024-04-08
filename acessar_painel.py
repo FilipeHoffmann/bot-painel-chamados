26c491738812ed65a031c229d3da7011511954ff
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-04-08 10:50:27 -0300

    g

--- acessar_painel.py
+++ acessar_painel.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import keyboard
 import sys
+import time
+import pyautogui
 
 class acessar_painel:
     def __init__(self,credenciais:tuple):
@@ -14,9 +16,13 @@ class acessar_painel:
         navegador.get(self.credeciais.get("url"))
         login = navegador.find_element(By.XPATH, '//*[@id="login.login"]')
         senha = navegador.find_element(By.XPATH, '//*[@id="login.senha"]')
+        time.sleep(3)
         login.send_keys(self.credeciais.get("login"))
+        time.sleep(3)
         senha.send_keys(self.credeciais.get("senha"))
+        time.sleep(3)
         entrar = navegador.find_element(By.XPATH, '//*[@id="entrar"]')
         entrar.click()
         navegador.get(self.credeciais.get("url"))
+        pyautogui.click("fechar_aviso.png")
         keyboard.wait("f11")
